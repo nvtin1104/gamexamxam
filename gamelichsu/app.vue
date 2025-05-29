7abd4ec3cc96ee8426fb18392546f312ab9f0071
Nguyễn Văn Tín <bguyt46@gmail.com>
2025-05-29 20:32:30 +0700

    feat: tạo header

--- gamelichsu/app.vue
+++ gamelichsu/app.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-// You might choose this based on an API call or logged-in status
 const layout = "default";
+import './app.css';
 </script>
 
 <template>
--- gamelichsu/pages/index.vue
+++ gamelichsu/pages/index.vue
@@ -1,3 +1,5 @@
 <template>
-    this is index page
+    <div class="bg-red color-primary h-screen flex items-center justify-center">
+        this is index page
+    </div>
 </template>
